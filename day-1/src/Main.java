f9ecb996639f4899e122b83d730c5a8ebd6af932
Oscar Wellner <oscar.wellner@gmail.com>
2022-12-03 12:43:14 +0100

    Adjust for part 2 of Day 1

--- day-1/src/Main.java
+++ day-1/src/Main.java
@@ -1,14 +1,18 @@
 import utils.SimpleFileReader;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        final int numberOfHighestTotalCaloriesToFind = 3;
+
         List<List<Integer>> collectionOfCaloriesPerElf = SimpleFileReader.readFile("input.txt");
+        List<Integer> totalCaloriesPerElf = new ArrayList<>();
+        int[] highestTotalCaloriesCollection = new int[numberOfHighestTotalCaloriesToFind];
 
         if (collectionOfCaloriesPerElf == null) return;
-
-        int highestCaloriesCount = 0;
 
         for (List<Integer> caloriesPerElf: collectionOfCaloriesPerElf) {
             int totalCalories = caloriesPerElf
@@ -16,9 +20,20 @@ public class Main {
                 .mapToInt(Integer::intValue)
                 .sum();
 
-            highestCaloriesCount = Math.max(highestCaloriesCount, totalCalories);
+            totalCaloriesPerElf.add(totalCalories);
         }
 
-        System.out.printf("The Elf which is carrying the highest total Calories, is carrying: %d Calories\n", highestCaloriesCount);
+        for (int i = 0; i < numberOfHighestTotalCaloriesToFind; i++) {
+            int highestTotalCalories = totalCaloriesPerElf.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .getAsInt();
+
+            totalCaloriesPerElf.remove(totalCaloriesPerElf.indexOf(highestTotalCalories));
+            highestTotalCaloriesCollection[i] = highestTotalCalories;
+        }
+        int totalCalories = Arrays.stream(highestTotalCaloriesCollection).sum();
+
+        System.out.printf("The number of calories that the top %d of Elves are carrying is a total of %d Calories.\n", numberOfHighestTotalCaloriesToFind, totalCalories);
     }
 }
