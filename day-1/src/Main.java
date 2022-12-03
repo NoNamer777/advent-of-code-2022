913c437b5cfa24349d4782329c347b77f9a54190
Oscar Wellner <oscar.wellner@gmail.com>
2022-12-03 13:09:19 +0100

    Extract file reading logic

--- day-1/src/Main.java
+++ day-1/src/Main.java
@@ -1,14 +1,18 @@
 import utils.SimpleFileReader;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         final int numberOfHighestTotalCaloriesToFind = 3;
 
-        List<List<Integer>> collectionOfCaloriesPerElf = SimpleFileReader.readFile("input.txt");
+        File file = new File(Objects.requireNonNull(Main.class.getClassLoader().getResource("input.txt")).getFile());
+
+        List<List<Integer>> collectionOfCaloriesPerElf = parseFileLines(SimpleFileReader.readFile(file));
         List<Integer> totalCaloriesPerElf = new ArrayList<>();
         int[] highestTotalCaloriesCollection = new int[numberOfHighestTotalCaloriesToFind];
 
@@ -35,5 +39,22 @@ public class Main {
         int totalCalories = Arrays.stream(highestTotalCaloriesCollection).sum();
 
         System.out.printf("The number of calories that the top %d of Elves are carrying is a total of %d Calories.\n", numberOfHighestTotalCaloriesToFind, totalCalories);
+    }
+
+    private static List<List<Integer>> parseFileLines(List<String> fileLines) {
+        if (fileLines == null) return null;
+
+        List<List<Integer>> collectionOfCaloriesPerElf = new ArrayList<>();
+        List<Integer> caloriesPerElf = new ArrayList<>();
+
+        for (String line: fileLines) {
+            if (line.isBlank() || line.isEmpty()) {
+                collectionOfCaloriesPerElf.add(caloriesPerElf);
+                caloriesPerElf = new ArrayList<>();
+                continue;
+            }
+            caloriesPerElf.add(Integer.parseInt(line));
+        }
+        return collectionOfCaloriesPerElf;
     }
 }
---
+++ src/utils/SimpleFileReader.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleFileReader {
+    public static List<String> readFile(File file) {
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader(file));
+
+            List<String> lines = new ArrayList<>();
+
+            String line = fileReader.readLine();
+
+            do {
+                lines.add(line);
+
+                line = fileReader.readLine();
+            }
+            while (line != null);
+
+            return lines;
+        }
+        catch (IOException | NullPointerException exception) {
+            System.out.printf("Something unexpected happened while trying to read file: '%s'\n", file.getPath());
+            System.out.println(exception.getMessage());
+        }
+        return null;
+    }
+}
