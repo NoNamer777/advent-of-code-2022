01f49793d8b3595f7ca5888a5f8ad158b36b1d94
Oscar Wellner <oscar.wellner@gmail.com>
2022-12-04 11:41:32 +0100

    Adjust day 2 for part 2

--- day-2/src/Main.java
+++ day-2/src/Main.java
@@ -7,42 +7,40 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Main {
-    private static final char CODE_OPPONENT_ROCK = 'A';
-    private static final char CODE_OPPONENT_PAPER = 'B';
-    private static final char CODE_OPPONENT_SCISSORS = 'C';
-    private static final char CODE_RESPONSE_ROCK = 'X';
-    private static final char CODE_RESPONSE_PAPER = 'Y';
-    private static final char CODE_RESPONSE_SCISSORS = 'Z';
+    private static final char MOVE_ROCK = 'A';
+    private static final char MOVE_PAPER = 'B';
+    private static final char MOVE_SCISSORS = 'C';
 
     private static final Map<Character, Integer> SCORE_BONUS_PER_MOVE = Map.of(
-        CODE_RESPONSE_ROCK, 1,
-        CODE_RESPONSE_PAPER, 2,
-        CODE_RESPONSE_SCISSORS, 3
+        MOVE_ROCK, 1,
+        MOVE_PAPER, 2,
+        MOVE_SCISSORS, 3
     );
 
-    private static final int SCORE_TIE = 3;
-    private static final int SCORE_WIN = 6;
+    private static final char PREDICTED_ROUND_STATE_LOSE = 'X';
+    private static final char PREDICTED_ROUND_STATE_TIE = 'Y';
+    private static final char PREDICTED_ROUND_STATE_WIN = 'Z';
+
+    private static final Map<Character, Integer> SCORE_BONUS_PER_OUTCOME_ROUND = Map.of(
+        PREDICTED_ROUND_STATE_LOSE, 0,
+        PREDICTED_ROUND_STATE_TIE, 3,
+        PREDICTED_ROUND_STATE_WIN, 6
+    );
 
     public static void main(String... inputs) {
         File file = new File(Objects.requireNonNull(Main.class.getClassLoader().getResource("rock-paper-scissors-tournament.txt")).getFile());
+        List<char[]> roundOutcomePredictions = parseFileLines(SimpleFileReader.readFile(file));
+
+        if (roundOutcomePredictions == null) return;
+
         int score = 0;
 
-        List<char[]> outcomeRounds = parseFileLines(SimpleFileReader.readFile(file));
-
-        if (outcomeRounds == null) return;
-
-        for (char[] round: outcomeRounds) {
+        for (char[] round: roundOutcomePredictions) {
             char opponentMove = round[0];
-            char move = round[1];
+            char roundOutcome = round[1];
+            char predictedMove = predictMoveOfRound(opponentMove, roundOutcome);
 
-            score += SCORE_BONUS_PER_MOVE.get(move);
-
-            if (isTie(opponentMove, move)) {
-                score += SCORE_TIE;
-            }
-            else if (isWin(opponentMove, move)) {
-                score += SCORE_WIN;
-            }
+            score += SCORE_BONUS_PER_MOVE.get(predictedMove) + SCORE_BONUS_PER_OUTCOME_ROUND.get(roundOutcome);
         }
         System.out.printf("My total score of this tournament is: %d\n", score);
     }
@@ -63,15 +61,25 @@ public class Main {
         return outcomeRounds;
     }
 
-    private static boolean isTie(char opponentMove, char move) {
-        return (opponentMove == CODE_OPPONENT_ROCK && move == CODE_RESPONSE_ROCK)
-            || (opponentMove == CODE_OPPONENT_PAPER && move == CODE_RESPONSE_PAPER)
-            || (opponentMove == CODE_OPPONENT_SCISSORS && move == CODE_RESPONSE_SCISSORS);
-    }
-
-    private static boolean isWin(char opponentMove, char move) {
-        return (move == CODE_RESPONSE_ROCK && opponentMove == CODE_OPPONENT_SCISSORS)
-            || (move == CODE_RESPONSE_PAPER && opponentMove == CODE_OPPONENT_ROCK)
-            || (move == CODE_RESPONSE_SCISSORS && opponentMove == CODE_OPPONENT_PAPER);
+    private static char predictMoveOfRound(char opponentMove, char roundOutcome) {
+        if (roundOutcome == PREDICTED_ROUND_STATE_TIE) {
+            return switch (opponentMove) {
+                case MOVE_ROCK -> MOVE_ROCK;
+                case MOVE_PAPER -> MOVE_PAPER;
+                default -> MOVE_SCISSORS;
+            };
+        }
+        if (roundOutcome == PREDICTED_ROUND_STATE_WIN) {
+            return switch (opponentMove) {
+                case MOVE_ROCK -> MOVE_PAPER;
+                case MOVE_PAPER -> MOVE_SCISSORS;
+                default -> MOVE_ROCK;
+            };
+        }
+        return switch (opponentMove) {
+            case MOVE_ROCK -> MOVE_SCISSORS;
+            case MOVE_PAPER -> MOVE_ROCK;
+            default -> MOVE_PAPER;
+        };
     }
 }
